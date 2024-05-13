40b8855a159af927c13a1600c4d14489ca80b8ee
Mike <kononov.mixa2045@gmail.com>
2024-05-13 13:50:57 +0500

    bert

--- data_handler.py
+++ data_handler.py
@@ -1,20 +1,44 @@
 import numpy as np
 import pandas as pd
+from collections import defaultdict
 
 
-def vectorize_seq(seq: np.ndarray, dimension=10000):
+def num_seq(seq):
+    words = defaultdict(int)
+    for row in seq:
+        for word in row:
+            words[word] += 1
+    print(len(words))
+
+
+def vectorize_seq(seq, dimension=10000):
     results = np.zeros((len(seq), dimension))
-    for i, seq in enumerate(seq):
-        results[i, seq] = 1
+    for i, new_seq in enumerate(seq):
+        print(new_seq)
+        results[i, new_seq] = 1
     return results
 
 
 def main():
-    train_data = pd.read_csv('data/train.csv')
-    test_data = pd.read_csv('data/test.csv')
+    train_data_p = pd.read_csv('data/train.csv')
+    test_data_p = pd.read_csv('data/test.csv')
 
-    x_train = vectorize_seq(train_data)
-    y_train = vectorize_seq(test_data)
+    train_data = train_data_p["text"].to_numpy()
+    train_labels = train_data_p["rating"].to_numpy()
+
+    seq = [[1, 2, 3], [4, 5, 6, 7], [8, 9]]
+    vectorized_seq = vectorize_seq(seq, dimension=10)
+
+    print(vectorized_seq)
+    num_seq(train_data)
+    v = []
+    for td in train_data:
+        v.append(np.array(td.split()))
+
+    x_train = vectorize_seq(v)
+    # y_train = vectorize_seq(train_labels)
+
+    print(x_train)
 
 
 if __name__ == '__main__':
